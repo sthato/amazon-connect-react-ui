3a9993f0164a586042cecb9ffbe1cd3cafeb0d0a
Thato Molahlwe Shebe <thato@cxengage.co.za>
2024-03-04 10:27:24 +0200

    refactor - add additional settings to ccp

--- src/App.js
+++ src/App.js
@@ -6,39 +6,66 @@ import "./App.css";
 function App() {
   const containerDiv = useRef();
 
-  
-
-  useEffect( () => {
+  useEffect(() => {
     console.log("Mounted");
 
     try {
       // eslint-disable-next-line no-undef
       connect.core.initCCP(containerDiv.current, {
         ccpUrl: "https://democxengage.my.connect.aws/ccp-v2/",
-        loginPopup: true,
+        loginPopup: true,               // optional, defaults to `true`
+          loginPopupAutoClose: true,      // optional, defaults to `false`
+          loginOptions: {                 // optional, if provided opens login in new window
+            autoClose: true,              // optional, defaults to `false`
+            height: 600,                  // optional, defaults to 578
+            width: 400,                   // optional, defaults to 433
+            top: 0,                       // optional, defaults to 0
+            left: 0                       // optional, defaults to 0
+          },
+          region: "eu-central-1",         // REQUIRED for `CHAT`, optional otherwise
+          softphone: {                    // optional, defaults below apply if not provided
+            allowFramedSoftphone: true,   // optional, defaults to false
+            disableRingtone: false,       // optional, defaults to false
+            ringtoneUrl: "/dj_hi_hat_sample.mp3", // optional, defaults to CCP’s default ringtone if a falsy value is set
+            allowFramedVideoCall: true,    // optional, default to false
+            allowEarlyGum: true    //optional, default to true
+          },
+          pageOptions: { //optional
+            enableAudioDeviceSettings: true, //optional, defaults to 'false'
+            enableVideoDeviceSettings: true, //optional, defaults to 'false'
+            enablePhoneTypeSettings: true //optional, defaults to 'true' 
+          },
+          shouldAddNamespaceToLogs: true, //optional, defaults to 'false'
+          ccpAckTimeout: 5000, //optional, defaults to 3000 (ms)
+          ccpSynTimeout: 3000, //optional, defaults to 1000 (ms)
+          ccpLoadTimeout: 10000 //optional, defaults to 5000 (ms)
       });
     } catch (error) {
       console.error(error);
     }
   }, []);
 
-  useEffect( () => {
-    async function createWidget(){
-      createDetailsWidget().then(widget => {
-        function onCustomerProfile(profile) {
-          console.log(profile)
-        }
-      
-        widget.on("customer_profile", onCustomerProfile);
-        widget.off("customer_profile", onCustomerProfile);
-      });
-    }
-    createWidget()
-  }, [])
+  const onCustomerProfile = function (profile) {
+    console.log("Customer Profile", profile);
+  };
+  const createWidget = async function () {
+    createDetailsWidget().then((widget) => {
+      widget.on("customer_profile", onCustomerProfile);
+      widget.off("customer_profile", onCustomerProfile);
+    });
+  };
+
+  useEffect(() => {
+    createWidget();
+  }, []);
 
   return (
     <div className="App">
-      <div id="container-div" ref={containerDiv} style={{width: "400px", height:"600px"}}></div>
+      <div
+        id="container-div"
+        ref={containerDiv}
+        style={{ width: "400px", height: "600px" }}
+      ></div>
     </div>
   );
 }
