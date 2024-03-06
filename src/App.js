6b083a61a290e3587855ec2454dc5acd18b18f83
Thato Molahlwe Shebe <thato@cxengage.co.za>
2024-03-06 09:00:21 +0200

    add - livechat widgets

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import "amazon-connect-streams";
-import { createDetailsWidget } from "@livechat/agent-app-sdk";
+import {
+  createDetailsWidget,
+  createMessageBoxWidget,
+  createFullscreenWidget,
+} from "@livechat/agent-app-sdk";
 import { useEffect, useRef } from "react";
 import "./App.css";
 
@@ -13,32 +17,35 @@ function App() {
       // eslint-disable-next-line no-undef
       connect.core.initCCP(containerDiv.current, {
         ccpUrl: "https://democxengage.my.connect.aws/ccp-v2/",
-        loginPopup: true,               // optional, defaults to `true`
-          loginPopupAutoClose: true,      // optional, defaults to `false`
-          loginOptions: {                 // optional, if provided opens login in new window
-            autoClose: true,              // optional, defaults to `false`
-            height: 600,                  // optional, defaults to 578
-            width: 350,                   // optional, defaults to 433
-            top: 0,                       // optional, defaults to 0
-            left: 0                       // optional, defaults to 0
-          },
-          region: "af-south-1",         // REQUIRED for `CHAT`, optional otherwise
-          softphone: {                    // optional, defaults below apply if not provided
-            allowFramedSoftphone: true,   // optional, defaults to false
-            disableRingtone: false,       // optional, defaults to false
-            // ringtoneUrl: "/dj_hi_hat_sample.mp3", // optional, defaults to CCP’s default ringtone if a falsy value is set
-            allowFramedVideoCall: true,    // optional, default to false
-            allowEarlyGum: true    //optional, default to true
-          },
-          pageOptions: { //optional
-            enableAudioDeviceSettings: true, //optional, defaults to 'false'
-            enableVideoDeviceSettings: true, //optional, defaults to 'false'
-            enablePhoneTypeSettings: true //optional, defaults to 'true' 
-          },
-          shouldAddNamespaceToLogs: true, //optional, defaults to 'false'
-          ccpAckTimeout: 5000, //optional, defaults to 3000 (ms)
-          ccpSynTimeout: 3000, //optional, defaults to 1000 (ms)
-          ccpLoadTimeout: 10000 //optional, defaults to 5000 (ms)
+        loginPopup: true, // optional, defaults to `true`
+        loginPopupAutoClose: true, // optional, defaults to `false`
+        loginOptions: {
+          // optional, if provided opens login in new window
+          autoClose: true, // optional, defaults to `false`
+          height: 600, // optional, defaults to 578
+          width: 350, // optional, defaults to 433
+          top: 0, // optional, defaults to 0
+          left: 0, // optional, defaults to 0
+        },
+        region: "af-south-1", // REQUIRED for `CHAT`, optional otherwise
+        softphone: {
+          // optional, defaults below apply if not provided
+          allowFramedSoftphone: true, // optional, defaults to false
+          disableRingtone: false, // optional, defaults to false
+          // ringtoneUrl: "/dj_hi_hat_sample.mp3", // optional, defaults to CCP’s default ringtone if a falsy value is set
+          allowFramedVideoCall: true, // optional, default to false
+          allowEarlyGum: true, //optional, default to true
+        },
+        pageOptions: {
+          //optional
+          enableAudioDeviceSettings: true, //optional, defaults to 'false'
+          enableVideoDeviceSettings: true, //optional, defaults to 'false'
+          enablePhoneTypeSettings: true, //optional, defaults to 'true'
+        },
+        shouldAddNamespaceToLogs: true, //optional, defaults to 'false'
+        ccpAckTimeout: 5000, //optional, defaults to 3000 (ms)
+        ccpSynTimeout: 3000, //optional, defaults to 1000 (ms)
+        ccpLoadTimeout: 10000, //optional, defaults to 5000 (ms)
       });
     } catch (error) {
       console.error(error);
@@ -46,17 +53,34 @@ function App() {
   }, []);
 
   const onCustomerProfile = function (profile) {
-    console.log("Customer Profile", profile);
-  };
-  const createWidget = async function () {
-    createDetailsWidget().then((widget) => {
-      widget.on("customer_profile", onCustomerProfile);
-      widget.off("customer_profile", onCustomerProfile);
-    });
+    console.log("Profile", profile);
   };
 
   useEffect(() => {
-    createWidget();
+    console.log("Loading widget");
+    let widget, fWidget, mWidget;
+    createDetailsWidget().then((w) => {
+      console.log("Loaded widget");
+      widget = w;
+      widget.on("customer_profile", onCustomerProfile);
+    });
+
+    createMessageBoxWidget.then((w) => {
+      console.log("Loaded MessageBoxWidget");
+      fWidget = w;
+    });
+
+    createFullscreenWidget.then((w) => {
+      console.log("Loaded FullscreenWidget");
+      mWidget = w;
+    });
+
+    return () => {
+      if (widget) {
+        console.log("Cleanup widget");
+        widget.off("customer_profile", onCustomerProfile);
+      }
+    };
   }, []);
 
   return (
